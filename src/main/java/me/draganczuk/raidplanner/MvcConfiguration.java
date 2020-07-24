7b9220914abe429bfc3ea3cd037565d187a24533
Przemek Dragańczuk <admin@draganczuk.tk>
2020-07-24 19:34:43 +0200

    Created barebones project skeleton with MVC and security

---
+++ src/main/java/me/draganczuk/raidplanner/MvcConfiguration.java
@@ -0,0 +1,16 @@
+package me.draganczuk.raidplanner;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
